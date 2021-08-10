3dae238f86d461679f66a0f9f322059fcd51187b
Rejo Mathew <rejopmathw@gmail.com>
2021-08-10 18:28:46 +0530

    Setting react router dom in redux application (basic)

--- src/index.js
+++ src/index.js
@@ -1,15 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Test from './components/GamesContainer';
 import { Provider } from 'react-redux'
 import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk'
 import rootReducer from '../src/reducer';
+import DragNDrop from './components/GamesContainer';
+import GameComp from './components/Games/TakeABreak';
+import {BrowserRouter as Router,Route,Switch} from 'react-router-dom';
 
 const store = createStore(rootReducer, compose(applyMiddleware(thunk))); 
 ReactDOM.render(
     <Provider store={store}>
-    <Test />
+    <Router>
+        <div>
+          <Switch>
+          <Route exact path="/" component={DragNDrop} />
+          <Route path="/game" component={GameComp} />
+          </Switch>  
+        </div>
+    </Router>
     </Provider>, 
     document.getElementById("root")
 );
--- webpack.config.js
+++ webpack.config.js
@@ -32,7 +32,8 @@ module.exports = {
     contentBase: path.join(__dirname, "public/"),
     port: 3000,
     publicPath: "http://localhost:3000/dist/",
-    hot: true
+    hot: true,
+    historyApiFallback: true
   },
   plugins: [new webpack.HotModuleReplacementPlugin()]
 };
